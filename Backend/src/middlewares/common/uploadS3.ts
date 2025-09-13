71157ce731625ac9948d2ed33f6161b41b0f9dfb
abdullhaks <aks4u786@gmail.com>
2025-09-13 10:54:59 +0530

    fix: aws credentials

--- Backend/src/middlewares/common/uploadS3.ts
+++ Backend/src/middlewares/common/uploadS3.ts
@@ -13,10 +13,6 @@ export const upload = multer({ storage });
 // S3 client config
 const s3Client = new S3Client({
   region: process.env.AWS_REGION!,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!,
-  },
 });
 
 
