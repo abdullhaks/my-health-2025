8fe3ce59b90e2327bbcb59d8efac800ca225ed5f
abdullhaks <aks4u786@gmail.com>
2026-01-02 14:21:59 +0530

    fixing: fixing mailing2

--- Backend/src/services/user/implementations/userAuthServices.ts
+++ Backend/src/services/user/implementations/userAuthServices.ts
@@ -35,18 +35,32 @@ import { z } from "zod";
 // });
 
 
+// const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: Number(process.env.SMTP_PORT),
+//   secure: false,
+//   auth: {
+//     user: process.env.SMTP_MAIL,
+//     pass: process.env.SMTP_PASSWORD,
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
+  host: `${process.env.SMTP_HOST}`,
   port: Number(process.env.SMTP_PORT),
   secure: false,
   auth: {
-    user: process.env.SMTP_MAIL,
-    pass: process.env.SMTP_PASSWORD,
+    user: `${process.env.SMTP_MAIL}`,
+    pass: `${process.env.SMTP_PASSWORD}`
   },
   tls: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
+
 
 
 @injectable()
